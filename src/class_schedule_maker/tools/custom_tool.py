59a7ea724733c8517ffbf3c97a7f18992b9fcdd7
MozartofCode <bertan.berker@gmail.com>
2024-11-13 18:56:15 -0500

    Created a custom tool for processing google calendar of a user, need to find the API calls next.

--- src/class_schedule_maker/tools/custom_tool.py
+++ src/class_schedule_maker/tools/custom_tool.py
@@ -3,8 +3,19 @@ from crewai_tools import BaseTool
 import pandas as pd
 from pydantic import BaseModel, Field
 
-
 # Custom tool to get the already existing (planned) calendar of a user for a semester
 
+class CalendarInput(BaseModel):
+    """Input schema for Calendar."""
+    argument: str = Field(..., description="Description of the argument.")
 
+class ProcessCalendar(BaseTool):
+    name: str = "Process Calendar"
+    description: str = (
+        "This tool processes the calendar to return the empty and non-empty slots of a user for that planned semester."
+    )
+    args_schema: Type[BaseModel] = CalendarInput
 
+    def _run(self, argument: str) -> str:
+        # Load the calendar
+        return
